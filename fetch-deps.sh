4a7c1c3b84bf9eab956fdc5b5c19728b6b290aeb
Jan de Muijnck-Hughes <jfdm@st-andrews.ac.uk>
2016-05-17 09:18:57 +0100

    Updates.
    Updated ipkg file format, and removed unused dependancy.

--- fetch-deps.sh
+++ fetch-deps.sh
@@ -6,16 +6,10 @@ cd dependancies/
 
 echo "Fetching Deps for Containers"
 
-echo "Fetching lightyear "
-git clone git@github.com:ziman/lightyear.git lightyear
-cd lightyear/
-make install
-cd ../
-
 echo "Fetching testing"
 git clone git@github.com:jfdm/idris-testing.git testing
 cd testing/
-make install
+idris --install test.ipkg
 cd ../
 
 cd ../
