0b2b47cc9f0ceb82eedc139491d2a59274c91051
Jan de Muijnck-Hughes <jan.deMuijnck-Hughes@glasgow.ac.uk>
2017-03-20 12:46:48 +0000

    Initial commit of travis job.

---
+++ default.nix
@@ -0,0 +1,11 @@
+# This is used in the Travis build to install the Idris compiler.
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+in {
+  idris-containers = stdenv.mkDerivation {
+    name = "idris-containers";
+    src = ./.;
+    buildInputs = with pkgs; [ haskellPackages.idris gmp ];
+  };
+}
